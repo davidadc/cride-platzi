83f15ad4205c0508dd159afb35fe0c557c9e777f
David Delgado <david@stracos.com>
2020-10-25 23:45:29 -0400

    Organizando modelos en un paquete de Django.

--- cride/users/models/users.py
+++ cride/users/models/users.py
@@ -3,6 +3,7 @@
 # Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Utilities
 from cride.utils.models import CRideModel
@@ -18,12 +19,18 @@ class User(CRideModel, AbstractUser):
     email = models.EmailField(
         'email address',
         unique=True,
-        error_message={
+        error_messages={
             'unique': 'A user with that email already exists.'
         }
     )
 
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message='Phone number must be entered in the format: +999999999. Up to 15 digits.'
+    )
+
     phone_number = models.CharField(
+        validators=[phone_regex],
         max_length=17,
         blank=True,
     )
@@ -45,3 +52,11 @@ class User(CRideModel, AbstractUser):
         default=True,
         help_text='Set to true when the user have verified its email address. '
     )
+
+    def __str__(self):
+        """Returns username."""
+        return self.username
+
+    def get_short_name(self):
+        """Returns username."""
+        return self.username
