13a0c74b208424f32e872c4d10b80788c2f83ac2
David Delgado <dadch13.14@gmail.com>
2020-10-27 00:09:24 -0400

    APIView.

---
+++ cride/users/urls.py
@@ -0,0 +1,11 @@
+"""Circles URLs."""
+
+# Django
+from django.urls import path
+
+# View
+from cride.users.views import UserLoginAPIView
+
+urlpatterns = [
+    path('users/login/', UserLoginAPIView.as_view(), name='login'),
+]
